5cfc1aae8737f231984952c4ae72cb39833492e0
serrabruna <brunaibserra@hotmail.com>
2026-06-26 16:15:45 -0300

    corrigindo chave do banco

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,10 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
+import 'dotenv/config';
 
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Erro: SUPABASE_URL ou SUPABASE_SERVICE_ROLE_KEY não foram definidos no .env");
