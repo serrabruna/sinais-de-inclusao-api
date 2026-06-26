1d55d4fe1c3e2552bc1e27e9877b442a915c707c
serrabruna <brunaibserra@hotmail.com>
2026-06-26 17:09:20 -0300

    correções

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,16 +1,14 @@
-import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
+import 'dotenv/config'; 
 
-dotenv.config();
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!supabaseUrl) {
-    console.error("ERRO: SUPABASE_URL não está definida!");
+    throw new Error("ERRO: Variável de ambiente SUPABASE_URL não está definida!");
 }
 if (!supabaseKey) {
-    console.error("ERRO: SUPABASE_SERVICE_ROLE_KEY não está definida!");
+    throw new Error("ERRO: Variável de ambiente SUPABASE_SERVICE_ROLE_KEY não está definida!");
 }
 
-export const supabase = createClient(supabaseUrl!, supabaseKey!);
+export const supabase = createClient(supabaseUrl, supabaseKey);
