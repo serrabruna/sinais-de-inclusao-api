13dc6ef2f0adaac11693c3b264992b5b4a4075ad
serrabruna <brunaibserra@hotmail.com>
2026-06-26 16:22:44 -0300

    verificação simples do banco de dados

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 import 'dotenv/config';
 
 dotenv.config();
-
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-    throw new Error("Erro: SUPABASE_URL ou SUPABASE_SERVICE_ROLE_KEY não foram definidos no .env");
+if (!supabaseUrl) {
+    console.error("ERRO: SUPABASE_URL não está definida!");
+}
+if (!supabaseKey) {
+    console.error("ERRO: SUPABASE_SERVICE_ROLE_KEY não está definida!");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl!, supabaseKey!);
